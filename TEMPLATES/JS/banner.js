4d0fc1b4cafd1dd7220455e0fe13edd355c892f1
RobinStone <vohuanrok@mail.ru>
2024-11-09 16:07:13 +0300

    banner updated

--- TEMPLATES/JS/banner.js
+++ TEMPLATES/JS/banner.js
@@ -1,15 +1,23 @@
 $(document).ready(function () {
+    let size_between = 15;
+    if(is_mobile()) {
+        size_between = 5;
+    }
+
+
     let swiper_banner = new Swiper('#banner', {
         direction: 'horizontal',
         loop: true,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
+        slidesPerView: 1.15, // Показываем 1.5 слайда (центрируем один, и видим часть соседних)
+        centeredSlides: true, // Центрируем активный слайд
+        spaceBetween: size_between, // Устанавливаем расстояние между слайдами
         navigation: {
             nextEl: '.swiper-button-next-banner',
             prevEl: '.swiper-button-prev-banner',
         },
         autoplay: {
-            delay: 6000, // Интервал в 3 секунды
+            delay: 6000, // Интервал в 6 секунд
+            disableOnInteraction: true, // Автопроигрывание не останавливается при взаимодействии
         },
     });
 });
